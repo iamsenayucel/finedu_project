6c636d14457a8cc059d10c69811382d6a5bc3d22
iamsenayucel <senayucelofficial@gmail.com>
2026-04-06 23:08:01 +0300

    sonner hatası düzeltildi

--- arayuz/src/main.tsx
+++ arayuz/src/main.tsx
@@ -1,11 +1,12 @@
 import { createRoot } from "react-dom/client";
 import App from "./app/App.tsx";
 import "./styles/index.css";
-import { Toaster } from "sonner";
+import { ThemeProvider } from "next-themes";
+import { Toaster } from "./app/components/ui/sonner";
 
 createRoot(document.getElementById("root")!).render(
-  <>
+  <ThemeProvider attribute="class" defaultTheme="light" disableTransitionOnChange>
     <App />
     <Toaster position="top-center" richColors closeButton />
-  </>
+  </ThemeProvider>
 );
